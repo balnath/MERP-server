40d2ce040246017b3cf6bbc14cbdf0cbdbd045f8
Divyanshu <divyanshu619@gmail.com>
2018-08-15 11:43:21 +0530

    Added support for logging

---
+++ src/test/java/com/merp/server/LoggingTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.merp.server;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ *
+ * @author Divyanshu
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoggingTest {
+    private static Logger logger = LogManager.getLogger();
+    
+    @Test
+    public void testLogging(){
+        logger.debug("This is a debug message");
+        logger.info("This is an info message");
+        logger.warn("This is a warn message");
+        logger.error("This is an error message");
+        logger.fatal("This is a fatal message");
+    }
+}
