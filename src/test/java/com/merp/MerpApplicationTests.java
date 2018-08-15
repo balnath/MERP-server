dae22b9bd79b1c54f396fa2e00ce0194e2884ea1
Divyanshu <divyanshu619@gmail.com>
2018-08-15 07:44:57 +0530

    Moved content to an upper folder

---
+++ src/test/java/com/merp/MerpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.merp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MerpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
