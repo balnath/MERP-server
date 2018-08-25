121b2dddf918aad1c8283cd4319313bfa424e78c
Divyanshu <divyanshu619@gmail.com>
2018-08-25 14:59:03 +0530

    #5 Added support to view, edit and remove users

--- src/main/java/com/merp/server/bootstrap/UserBootstrap.java
+++ src/main/java/com/merp/server/bootstrap/UserBootstrap.java
@@ -5,8 +5,10 @@
  */
 package com.merp.server.bootstrap;
 
+import com.merp.server.model.Department;
 import com.merp.server.model.Role;
 import com.merp.server.model.User;
+import com.merp.server.repository.DepartmentRepository;
 import com.merp.server.repository.RoleRepository;
 import com.merp.server.repository.UserRepository;
 import javax.annotation.PostConstruct;
@@ -28,6 +30,8 @@ public class UserBootstrap {
     @Inject
     RoleRepository roleRepository;
     @Inject
+    DepartmentRepository departmentRepository;
+    @Inject
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     private static final Logger logger = LogManager.getLogger();
@@ -41,14 +45,28 @@ public class UserBootstrap {
                 user = new User();
                 user.setUsername("admin");
                 user.setPassword(bCryptPasswordEncoder.encode("password"));
+                user.setName("Admin");
                 Role adminRole = roleRepository.findByName("ADMIN");
-                if(adminRole == null) {
+                if (adminRole == null) {
                     adminRole = new Role();
                     adminRole.setName("ADMIN");
                     adminRole.setDescription("Description for ADMIN Role");
                     roleRepository.save(adminRole);
                 }
                 user.setRole(adminRole);
+                String departmentName = "General";
+                Department department = departmentRepository.findByName(departmentName);
+                if (department == null) {
+                    logger.info("Adding Department : " + departmentName);
+                    department = new Department();
+                    department.setName(departmentName);
+                    department.setDescription("Description for " + departmentName);
+                    departmentRepository.save(department);
+                    logger.info("Department " + departmentName + " added to the database");
+                } else {
+                    logger.info("Department " + departmentName + " already present in the database");
+                }
+                user.setDepartment(department);
                 userRepository.save(user);
                 logger.info("Admin user added successfully !");
             } else {
--- src/main/java/com/merp/server/model/BaseModel.java
+++ src/main/java/com/merp/server/model/BaseModel.java
@@ -13,7 +13,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 @MappedSuperclass
 public class BaseModel {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private long createdBy;
